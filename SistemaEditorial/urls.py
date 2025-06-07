f16ec8b471042584be0645d93ece6ce48e8ec6b5
Zhekkai <86115394+Zhekkai@users.noreply.github.com>
2025-06-06 22:55:39 -0600

    agregación de rutas registro

--- SistemaEditorial/urls.py
+++ SistemaEditorial/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login, name='login'),
+    path('registro/', views.registro, name='registro')
 ]
