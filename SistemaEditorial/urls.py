9b1b1ec281bfb4e4fdb553f8b8209de528cc6d25
José Jiménez <josealas532@gmail.com>
2025-06-06 23:21:33 -0600

    cambio registro

--- SistemaEditorial/urls.py
+++ SistemaEditorial/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login, name='login'),
-    path('registro/', views.registro, name='registro')
+    path('registro/', views.registro, name='registro'),
 ]
--- SistemaEditorial/views.py
+++ SistemaEditorial/views.py
@@ -4,4 +4,4 @@ def login(request):
     return render(request, 'login.html', {})
 
 def registro(request):
-    return render(request, 'registro.html',{})
+    return render(request, 'registro.html', {})
