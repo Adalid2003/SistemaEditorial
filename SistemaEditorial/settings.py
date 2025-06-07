10a062fe2dbe55afd7b5280a9e668362bd7e8f30
José Jiménez <josealas532@gmail.com>
2025-06-06 20:16:28 -0600

    conexion a base de datos

--- SistemaEditorial/settings.py
+++ SistemaEditorial/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'SistemaEditorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'editorial_db',
+        'USER': 'postgres',
+        'PASSWORD': 'hdp115',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
